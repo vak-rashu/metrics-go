f65f6be0e508143f4889a2f8dacd2cfa5c405e87
vak-rashu <rashu.cloud11@gmail.com>
2026-08-20 08:25:51 +0000

    Basic UI Initialized

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import (
-	// "github.com/vak-rashu/metrics-go/cmd"
-	tui "github.com/vak-rashu/metrics-go/ui"
-)
+import tui "github.com/vak-rashu/metrics-go/ui"
 
 func main() {
 	// cmd.Execute()
--- ui/simple_page.go
+++ ui/simple_page.go
@@ -1,12 +1,32 @@
 package tui
 
 import (
+	gloss "charm.land/lipgloss/v2"
 	tea "github.com/charmbracelet/bubbletea"
+	metrics "github.com/vak-rashu/metrics-go/pkg"
 )
+
+var headLineStyle = gloss.NewStyle().
+	Width(100).
+	Align(gloss.Center).
+	Background(gloss.Color("3")).
+	Foreground(gloss.Color("12"))
+
+var tabStyle = gloss.NewStyle().
+	Border(gloss.RoundedBorder()).
+	PaddingLeft(1).
+	PaddingRight(1).
+	BorderForeground(gloss.Color("183"))
 
 // model data
 type simplePage struct {
 	msg string
+}
+
+var blocks = []string{
+	"CPU",
+	"Processes",
+	"Memory",
 }
 
 func newSimplePage(msg string) simplePage {
@@ -14,21 +34,43 @@ func newSimplePage(msg string) simplePage {
 }
 
 func (s simplePage) Init() tea.Cmd {
-	return nil
+	return func() tea.Msg {
+		gloss.Println(headLineStyle.Render(s.msg))
+		return nil
+	}
 }
 
 func (s simplePage) View() string {
-	return s.msg
+
+	listString := []string{}
+	for _, val := range blocks {
+		listString = append(listString, tabStyle.Render(val))
+	}
+	return gloss.JoinHorizontal(gloss.Bottom, listString...)
+
 }
 
 func (s simplePage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg.(type) {
 	case tea.KeyMsg:
 		switch msg.(tea.KeyMsg).String() {
-		case "q":
+		case "m":
+			return s, getCPUStat()
+		case "enter":
 			return s, tea.Quit
 		}
 	}
 
 	return s, nil
+}
+
+func getCPUStat() tea.Cmd {
+	return func() tea.Msg {
+		if cpu, err := metrics.ShowCPUstat(); err != nil {
+			return err
+		} else {
+			return cpu
+		}
+
+	}
 }
