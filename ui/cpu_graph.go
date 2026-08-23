29de5f6199baefac0ea010db5d5de580093eee88
vak-rashu <rashu.cloud11@gmail.com>
2026-08-23 08:45:57 +0000

    Remove redundant func createChart from cpu_graph.go

--- ui/cpu_graph.go
+++ ui/cpu_graph.go
@@ -1,16 +1,10 @@
 package tui
 
 import (
-	"fmt"
-	"os"
-	"time"
-
 	tea "charm.land/bubbletea/v2"
 	gloss "charm.land/lipgloss/v2"
-	booba "github.com/NimbleMarkets/go-booba"
 	tslc "github.com/NimbleMarkets/ntcharts/v2/linechart/timeserieslinechart"
 	zone "github.com/lrstanley/bubblezone/v2"
-	metrics "github.com/vak-rashu/metrics-go/pkg"
 )
 
 type model struct {
@@ -48,36 +42,4 @@ func (m model) View() tea.View {
 	v.MouseMode = tea.MouseModeCellMotion
 
 	return v
-}
-
-func createChart() {
-
-	width := 30
-	height := 12
-	chart := tslc.New(width, height)
-
-	zoneManager := zone.New()
-	chart.SetZoneManager(zoneManager)
-	chart.Focus()
-
-	cpu, _ := metrics.ShowCPUstat()
-	// if err != nil {
-	// 	return err
-	// }
-
-	dataSet := []float64{cpu.UserTime, cpu.NiceTime, cpu.SystemTime}
-	for i, v := range dataSet {
-		date := time.Now().Add(time.Hour * time.Duration(24*i))
-		chart.PushDataSet("dataset2", tslc.TimePoint{Time: date, Value: v})
-	}
-
-	chart.SetDataSetStyle("dataset2",
-		gloss.NewStyle().
-			Foreground(gloss.Color("10")))
-
-	m := model{chart, zoneManager}
-	if err := booba.Run(m); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
 }
