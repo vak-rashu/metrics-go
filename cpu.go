fcd91220b0871af91ad60878055c7dbe756ce19f
vak-rashu <rashu.cloud11@gmail.com>
2026-08-28 12:20:53 +0000

    Organise files

--- cpu.go
+++ cpu.go
@@ -1,7 +1,7 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
-package cmd
+package main
 
 // var (
 // 	isAll      string
--- root.go
+++ root.go
@@ -1,7 +1,7 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
-package cmd
+package main
 
 import (
 	"os"
