14c26a76805a95cf0f44287cef6f11b709ba19f4
vak-rashu <rashu.cloud11@gmail.com>
2026-09-09 05:46:00 +0000

    disk iops and throughput noticed

--- metrics/disk_io.go
+++ metrics/disk_io.go
@@ -1,1 +1,22 @@
 package metrics
+
+// disk io creates the graph of
+// number of reads and writes done
+
+// the values is taken from
+// {/proc/diskstats}
+// all metrics are cumulative
+// except field 9
+
+// values to consider for the graph
+// field 1- total number of reads completed.
+// field 4 and 10 reads the miliseconds it spent reading and writing-- keep it for the latency graph
+// field 5- total number of write completed.
+// this metrics is the read and write operations done per sec
+
+// field 3 and 7 gives the sectors written and read
+// this is also cumulative
+// these are the bytes read and written /sec
+// the disk throughput is this
+
+// identify how busy your disks are
