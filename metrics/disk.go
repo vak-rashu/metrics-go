94c11617094d26f5ca99773dce18af642fe486b3
vak-rashu <rashu.cloud11@gmail.com>
2026-09-10 18:29:53 +0000

    change files and folders

--- metrics/disk.go
+++
@@ -1,9 +0,0 @@
-package metrics
-
-// read real disks value only
-// ramN and loopN are virtual devices
-// func readProcDiskstat() {
-// 	path := procPath("diskstats")
-// 	reader, err := openPath(path)
-
-// }
--- process.go
+++ process.go
@@ -3,35 +3,35 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/spf13/cobra"
-	metrics "github.com/vak-rashu/metrics-go/metrics"
-)
+// 	"github.com/spf13/cobra"
+// 	metrics "github.com/vak-rashu/metrics-go/metrics"
+// )
 
-// processCmd represents the cpu command
-var processCmd = &cobra.Command{
-	Use:   "process",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+// // processCmd represents the cpu command
+// var processCmd = &cobra.Command{
+// 	Use:   "process",
+// 	Short: "A brief description of your command",
+// 	Long: `A longer description that spans multiple lines and likely contains examples
+// and usage of using your command. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+// Cobra is a CLI library for Go that empowers applications.
+// This application is a tool to generate the needed files
+// to quickly create a Cobra application.`,
 
-	Args: cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		list, processStat, err := metrics.ShowPerProcessData(args[0])
-		if err != nil {
-			return err
-		}
+// 	Args: cobra.ExactArgs(1),
+// 	RunE: func(cmd *cobra.Command, args []string) error {
+// 		list, processStat, err := metrics.ShowPerProcessData(args[0])
+// 		if err != nil {
+// 			return err
+// 		}
 
-		fmt.Println(list, processStat)
+// 		fmt.Println(list, processStat)
 
-		return nil
-	},
-}
+// 		return nil
+// 	},
+// }
 
-func init() {}
+// func init() {}
